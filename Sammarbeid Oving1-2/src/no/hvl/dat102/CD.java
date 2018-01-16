1be96d02ec271247f0c03e1ab254674800a207a1
H571531 <31210153+H571531@users.noreply.github.com>
2018-01-16 12:22:03 +0100

    CD impl

--- Sammarbeid Oving1-2/src/no/hvl/dat102/CD.java
+++ Sammarbeid Oving1-2/src/no/hvl/dat102/CD.java
@@ -1,5 +1,68 @@
 package no.hvl.dat102;
 
 public class CD {
+	private int cdNummer;
+	private String cdArtist;
+	private String cdTittel;
+	private int cdLansering;
+	private Sjanger cdSjanger;
+	
+	public CD() {
+		this(0,null,null,0,null);
+		}
 
+	public CD(int cdNummer,String cdArtist, String cdTittel,int cdLansering, Sjanger cdSjanger) {
+		this.cdNummer=cdNummer;
+		this.cdArtist=cdArtist;
+		this.cdTittel=cdTittel;
+		this.cdLansering=cdLansering;
+		this.cdSjanger=cdSjanger;	
+	}
+
+	public int getCdNummer() {
+		return cdNummer;
+	}
+
+	public void setCdNummer(int cdNummer) {
+		this.cdNummer = cdNummer;
+	}
+
+	public String getCdArtist() {
+		return cdArtist;
+	}
+
+	public void setCdArtist(String cdArtist) {
+		this.cdArtist = cdArtist;
+	}
+
+	public String getCdTittel() {
+		return cdTittel;
+	}
+
+	public void setCdTittel(String cdTittel) {
+		this.cdTittel = cdTittel;
+	}
+
+	public int getCdLansering() {
+		return cdLansering;
+	}
+
+	public void setCdLansering(int cdLansering) {
+		this.cdLansering = cdLansering;
+	}
+
+	public Sjanger getCdSjanger() {
+		return cdSjanger;
+	}
+
+	public void setCdSjanger(Sjanger cdSjanger) {
+		this.cdSjanger = cdSjanger;
+	}
+
+	@Override
+	public String toString() {
+		return "CD [cdNummer=" + cdNummer + ", cdArtist=" + cdArtist + ", cdTittel=" + cdTittel + ", cdLansering="
+				+ cdLansering + ", cdSjanger=" + cdSjanger + "]";
+	}
+	
 }

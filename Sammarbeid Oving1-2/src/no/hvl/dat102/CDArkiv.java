b48eb238fbd065f27b3f9d5fbb01f2dad25fde1a
tohov <tohov@158.37.225.36>
2018-01-16 12:51:06 +0100

    CDArkiv.leggTilCD()

--- Sammarbeid Oving1-2/src/no/hvl/dat102/CDArkiv.java
+++ Sammarbeid Oving1-2/src/no/hvl/dat102/CDArkiv.java
@@ -6,12 +6,23 @@ public class CDarkiv implements CDArkivADT {
 	private final static int STANDARD_KAPASITET = 100;
 	
 	private CD[] arkiv;
+	int antall;
 	
 	public CDarkiv() {
 		this(STANDARD_KAPASITET);
 	}
 	public CDarkiv(int storrelse) {
 		this.arkiv = new CD[storrelse];
+		antall = 0;
+	}
+	
+	@Override
+	public void leggTilCD(CD cd) {
+		if(antall == arkiv.length){
+			utvid();
+		}
+		arkiv[antall] = cd;
+		antall++;
 	}
 	
 }

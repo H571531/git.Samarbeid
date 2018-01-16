7aa7be9597ea86d03afbdc576085566527f1ae1f
Anders <Anders@158.37.225.225>
2018-01-16 14:26:35 +0100

    Fatal Error v3.0::

--- Sammarbeid Oving1-2/src/no/hvl/dat102/Tekstgrensesnitt.java
+++ Sammarbeid Oving1-2/src/no/hvl/dat102/Tekstgrensesnitt.java
@@ -1,5 +1,39 @@
 package no.hvl.dat102;
+import no.hvl.dat102.adt.CDArkivADT;
+import java.util.Scanner;
+
 
 public class Tekstgrensesnitt {
-
+	
+	
+	public static CD lesCD() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Skriv inn CD-Nummer");
+		String nummerInn = sc.nextLine();
+		System.out.println("Skriv inn artist-navn eller gruppe");
+		String navnInn = sc.nextLine();
+		System.out.println("Skriv inn tittel");
+		String tittelInn = sc.nextLine();
+		System.out.println("Skriv inn utgivelsesår (int)");
+		int utAar = sc.nextInt();
+		System.out.println("Skriv inn sjanger");
+		Sjanger sjanger = Sjanger.finnSjanger(sc.nextLine());
+		sc.close();
+	}
+	
+	public void visCD(CD cd) {
+		
+	}
+	
+	public void skrivUtCdDelstrengITittel(CDArkivADT cda, String delstreng) {
+		
+	}
+	
+	public void skrivUtCdArtist(CDArkivADT cda, String delstreng) {
+		
+	}
+	
+	public void skrivUtStatistikk(CDArkivADT cda) {
+		
+	}
 }

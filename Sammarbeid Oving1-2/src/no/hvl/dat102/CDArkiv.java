93a21db90b5212eef89442d95fd391cd85a8ab69
tohov <tohov@158.37.225.36>
2018-01-16 12:44:53 +0100

    CDArkiv.STANDARD_KAPASITET

--- Sammarbeid Oving1-2/src/no/hvl/dat102/CDArkiv.java
+++ Sammarbeid Oving1-2/src/no/hvl/dat102/CDArkiv.java
@@ -3,10 +3,12 @@ import no.hvl.dat102.adt.CDArkivADT;
 
 public class CDarkiv implements CDArkivADT {
 	
+	private final static int STANDARD_KAPASITET = 100;
+	
 	private CD[] arkiv;
 	
 	public CDarkiv() {
-		this(0);
+		this(STANDARD_KAPASITET);
 	}
 	public CDarkiv(int storrelse) {
 		this.arkiv = new CD[storrelse];

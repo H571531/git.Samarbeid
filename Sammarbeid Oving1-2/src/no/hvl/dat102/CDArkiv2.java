38e9f33740b3445adac47ae3cea5d868edd223e5
tohov <tohov@158.37.224.206>
2018-01-23 13:14:57 +0100

    CDArkiv2 med tomme metoder

---
+++ Sammarbeid Oving1-2/src/no/hvl/dat102/CDArkiv2.java
@@ -0,0 +1,49 @@
+package no.hvl.dat102;
+
+import no.hvl.dat102.adt.CDArkivADT;
+
+public class CDArkiv2 implements CDArkivADT {
+
+	@Override
+	public CD[] getCDTabell() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void leggTilCD(CD nyCd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean slettCD(int CDNummer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public CD[] sokTittel(String delAvTittel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CD[] sokArtist(String artist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getAntall() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getAntall(Sjanger sjanger) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

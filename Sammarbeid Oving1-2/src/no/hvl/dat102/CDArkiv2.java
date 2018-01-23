d9425fe5e72e4c022b2196cfb988aaa977ced739
tohov <tohov@158.37.224.206>
2018-01-23 14:10:48 +0100

    CDArkiv2.slettCD og CDArkiv2.sokTittel

--- Sammarbeid Oving1-2/src/no/hvl/dat102/CDArkiv2.java
+++ Sammarbeid Oving1-2/src/no/hvl/dat102/CDArkiv2.java
@@ -3,6 +3,14 @@ package no.hvl.dat102;
 import no.hvl.dat102.adt.CDArkivADT;
 
 public class CDArkiv2 implements CDArkivADT {
+	
+	private int antall;
+	private LinearNode<CD> start;
+	
+	public CDArkiv2() {
+		antall = 0;
+		start = null;
+	}
 
 	@Override
 	public CD[] getCDTabell() {
@@ -18,14 +26,77 @@ public class CDArkiv2 implements CDArkivADT {
 
 	@Override
 	public boolean slettCD(int CDNummer) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		//return false hvis arkivet er tomt
+		if(antall == 0) {
+			return false;
+		}
+		boolean slettet = false;
+		
+		LinearNode<CD> forrige = null;
+		LinearNode<CD> denne = start;
+		
+		
+		//Spesielt tilfelle hvis første skal slettes
+		if(denne.getElement().getCdNummer() == CDNummer) {
+			start = start.getNeste();
+			antall--;
+			slettet = true;
+		} else {
+			denne = denne.getNeste();
+			forrige = start;
+			
+			while(!slettet && denne != null) {
+				if(denne.getElement().getCdNummer() == CDNummer) {
+					//Hvis match, sett forriges neste-referanse til dennes neste-referanse
+					forrige.setNeste(denne.getNeste());
+					antall--;
+					slettet = true;
+				} else {
+					//Hvis ikke match, oppdater forrige og denne
+					forrige = denne;
+					denne = denne.getNeste();
+				}
+			}
+			
+		}
+		return slettet;
+		
+		
 	}
 
+	//Samme private TrimTab() som i CDArkiv
+	private CD[] trimTab(CD[] tab, int n) {
+		CD[] cdtab2 = new CD[n];
+		int i = 0;
+		while (i < n) {
+			cdtab2[i] = tab[i];
+			i++;
+		}
+		return cdtab2;
+		
+	}
+	
 	@Override
 	public CD[] sokTittel(String delAvTittel) {
-		// TODO Auto-generated method stub
-		return null;
+		CD[] ut = new CD[antall];
+		int antallFunnet = 0;
+		LinearNode<CD> denne = start;
+		
+		while(denne != null) {
+			if(denne.getElement().getCdTittel().contains(delAvTittel)) {
+				ut[antallFunnet] = denne.getElement();
+				antallFunnet++;
+			}
+		}
+		
+		//return null hvis det ikke er funnet noen
+		if(antallFunnet == 0) {
+			return null;
+		}
+		
+		return trimTab(ut, antallFunnet);
+		
 	}
 
 	@Override
